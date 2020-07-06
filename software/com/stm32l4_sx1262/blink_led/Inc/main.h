f34d8017727de192e689a94a798d2eebd503499a
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2020-07-06 14:03:31 +0200

    blinky example updated (compatibility with STM32CubeIDE and new FlockLab 2 target adaptor)

--- software/com/stm32l4_sx1262/blink_led/Inc/main.h
+++ software/com/stm32l4_sx1262/blink_led/Inc/main.h
@@ -34,44 +34,50 @@
 #define __MAIN_H
 
 
-#define USE_LL_DRIVERS    1
+#define USE_SWD                     0         /* set to 1 to use SWD port (SWDIO and SWDCLK pins) for debugging */
+#define USE_SWO                     0         /* set to 1 to use SWO pin for data tracing (DWT) */
+#define CPU_SPEED                   80000000
+#define UART_BAUDRATE               115200
+
+#define LOW_POWER_MODE              PWR_CR1_LPMS_STOP2    /* other possible values: PWR_CR1_LPMS_STANDBY, PWR_CR1_LPMS_SHUTDOWN */
+
+/* GREEN LED */
+#define LED1_PIN                    GPIO_PIN_8
+#define LED1_GPIO_PORT              GPIOB
+/* RED LED */
+#define LED2_PIN                    GPIO_PIN_9
+#define LED2_GPIO_PORT              GPIOB
+#define LED1_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOB_CLK_ENABLE()
+#define LED1_GPIO_CLK_DISABLE()     __HAL_RCC_GPIOB_CLK_DISABLE()
+
+#define FLOCKLAB_LED1_PIN           GPIO_PIN_3
+#define FLOCKLAB_LED1_PORT          GPIOH
+#define FLOCKLAB_LED2_PIN           GPIO_PIN_3
+#define FLOCKLAB_LED2_PORT          GPIOB
+#define FLOCKLAB_LED3_PIN           GPIO_PIN_13
+#define FLOCKLAB_LED3_PORT          GPIOA
+#define FLOCKLAB_INT1_PIN           GPIO_PIN_3
+#define FLOCKLAB_INT1_PORT          GPIOA
+#define FLOCKLAB_INT2_PIN           GPIO_PIN_14
+#define FLOCKLAB_INT2_PORT          GPIOA
+#define FLOCKLAB_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOA_CLK_ENABLE(); __HAL_RCC_GPIOB_CLK_ENABLE(); __HAL_RCC_GPIOH_CLK_ENABLE()
+#define FLOCKLAB_GPIO_CLK_DISABLE() __HAL_RCC_GPIOA_CLK_DISABLE(); __HAL_RCC_GPIOB_CLK_DISABLE(); __HAL_RCC_GPIOH_CLK_DISABLE()
+
+#define UART_RXD_PIN                GPIO_PIN_10
+#define UART_RXD_PORT               GPIOA
+#define UART_TXD_PIN                GPIO_PIN_9
+#define UART_TXD_PORT               GPIOA
+#define UART_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
 
 
 #include "stm32l4xx.h"
-
-#if USE_LL_DRIVERS
 #include "stm32l4xx_ll_system.h"
 #include "stm32l4xx_ll_bus.h"
 #include "stm32l4xx_ll_rcc.h"
 #include "stm32l4xx_ll_gpio.h"
 #include "stm32l4xx_ll_utils.h"
 #include "stm32l4xx_ll_usart.h"
-#endif
-
-#include "stm32l4xx_hal.h"      // includes hal config
-
-/* GREEN LED */
-#define LED1_PIN                                GPIO_PIN_8
-#define LED1_GPIO_PORT                          GPIOB
-/* RED LED */
-#define LED2_PIN                                GPIO_PIN_9
-#define LED2_GPIO_PORT                          GPIOB
-#define LED1_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOB_CLK_ENABLE()
-#define LED1_GPIO_CLK_DISABLE()                 __HAL_RCC_GPIOB_CLK_DISABLE()
-
-#define FLOCKLAB_LED1_PIN                       GPIO_PIN_14
-#define FLOCKLAB_LED2_PIN                       GPIO_PIN_15         // note: also used for SWCLK
-#define FLOCKLAB_LED3_PIN                       GPIO_PIN_13         // note: also used for SWDIO
-#define FLOCKLAB_INT1_PIN                       GPIO_PIN_3
-#define FLOCKLAB_GPIO_PORT                      GPIOA
-#define FLOCKLAB_GPIO_CLK_ENABLE()              __HAL_RCC_GPIOA_CLK_ENABLE()
-#define FLOCKLAB_GPIO_CLK_DISABLE()             __HAL_RCC_GPIOA_CLK_DISABLE()
-
-#define UART_RXD_PIN                            GPIO_PIN_10
-#define UART_RXD_PORT                           GPIOA
-#define UART_TXD_PIN                            GPIO_PIN_9
-#define UART_TXD_PORT                           GPIOA
-#define UART_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#include "stm32l4xx_hal.h"        /* includes hal config */
 
 
 void NMI_Handler(void);
--- software/com/stm32l4_sx1262/blink_led/Src/main.c
+++ software/com/stm32l4_sx1262/blink_led/Src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Swiss Federal Institute of Technology (ETH Zurich).
+ * Copyright (c) 2020, Swiss Federal Institute of Technology (ETH Zurich).
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,7 +30,7 @@
  * Author:  Reto Da Forno
  */
 
-/* a simple blink LED example for the STM32L443 MCU (LoRa Comboard) */
+/* a simple blink LED example for the STM32L443 MCU (DPP2 LoRa Comboard on FlockLab 2) */
 
 #include "main.h"
 
@@ -40,10 +40,17 @@ void gpio_init(void);
 void uart_init(void);
 void uart_println(const char* str);
 void delay(uint32_t loops);
+void test_lpm(void);
+
+
+static volatile uint32_t counter = 0;
 
 
 int main(void)
 {
+  //test_lpm();
+
+  /* initialize clocks, pins and peripherals */
   clock_init();
   gpio_init();
   uart_init();
@@ -53,17 +60,96 @@ int main(void)
 
   while (1)
   {
+    /* toggle the green and red LED */
     LL_GPIO_TogglePin(LED1_GPIO_PORT, LED1_PIN);
     LL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
-    //LL_GPIO_TogglePin(FLOCKLAB_GPIO_PORT, FLOCKLAB_LED1_PIN);
-    LL_GPIO_TogglePin(FLOCKLAB_GPIO_PORT, FLOCKLAB_LED2_PIN);
-    //LL_GPIO_TogglePin(FLOCKLAB_GPIO_PORT, FLOCKLAB_LED3_PIN);
-    LL_GPIO_TogglePin(FLOCKLAB_GPIO_PORT, FLOCKLAB_INT1_PIN);
+
+    /* toggle all FlockLab tracing pins */
+    LL_GPIO_TogglePin(FLOCKLAB_LED1_PORT, FLOCKLAB_LED1_PIN);
+  #if !USE_SWO
+    LL_GPIO_TogglePin(FLOCKLAB_LED2_PORT, FLOCKLAB_LED2_PIN);
+  #endif /* USE_SWO */
+  #if !USE_SWD
+    LL_GPIO_TogglePin(FLOCKLAB_LED3_PORT, FLOCKLAB_LED3_PIN);
+    LL_GPIO_TogglePin(FLOCKLAB_INT2_PORT, FLOCKLAB_INT2_PIN);
+  #endif /* USE_SWD */
+    LL_GPIO_TogglePin(FLOCKLAB_INT1_PORT, FLOCKLAB_INT1_PIN);
 
     uart_println("hello world!");
+    counter++;
 
-    delay(SystemCoreClock / 10);
+    delay(SystemCoreClock / 10);    /* wait ~1s */
   }
+}
+
+
+void test_lpm(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* configure all GPIOs for minimal current drain */
+
+  /* enable clocks */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+
+  /* apply a default config (ensures that all unused / unconnected pins are in analog mode) */
+  GPIO_InitStruct.Pin  = GPIO_PIN_All;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /* configure all pins that are connected to an external circuit as inputs with pull resistors where necessary */
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_0 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_11 | GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_12;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_13;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin  = GPIO_PIN_3;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /* disable clocks */
+  __HAL_RCC_GPIOA_CLK_DISABLE();
+  __HAL_RCC_GPIOB_CLK_DISABLE();
+  __HAL_RCC_GPIOC_CLK_DISABLE();
+  __HAL_RCC_GPIOH_CLK_DISABLE();
+
+  /* enter low-power mode */
+  MODIFY_REG(PWR->CR1, PWR_CR1_LPMS, LOW_POWER_MODE);
+  SET_BIT(SCB->SCR, ((uint32_t)SCB_SCR_SLEEPDEEP_Msk));
+  __WFI();
 }
 
 
@@ -73,11 +159,17 @@ void gpio_init(void)
   FLOCKLAB_GPIO_CLK_ENABLE();
   LL_GPIO_SetPinMode(LED1_GPIO_PORT, LED1_PIN, LL_GPIO_MODE_OUTPUT);
   LL_GPIO_SetPinMode(LED2_GPIO_PORT, LED2_PIN, LL_GPIO_MODE_OUTPUT);
-  /* Note: only use FLOCKLAB_LED1 and FLOCKLAB_LED3 pins if debugging is not required (SWDIO, SWCLK)! */
-  //LL_GPIO_SetPinMode(FLOCKLAB_GPIO_PORT, FLOCKLAB_LED1_PIN, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(FLOCKLAB_GPIO_PORT, FLOCKLAB_LED2_PIN, LL_GPIO_MODE_OUTPUT);
-  //LL_GPIO_SetPinMode(FLOCKLAB_GPIO_PORT, FLOCKLAB_LED3_PIN, LL_GPIO_MODE_OUTPUT);
-  LL_GPIO_SetPinMode(FLOCKLAB_GPIO_PORT, FLOCKLAB_INT1_PIN, LL_GPIO_MODE_OUTPUT);
+  LL_GPIO_SetPinMode(FLOCKLAB_LED1_PORT, FLOCKLAB_LED1_PIN, LL_GPIO_MODE_OUTPUT);
+#if !USE_SWO
+  /* do not configure the SWO pin as output if SWO is used to output debug information */
+  LL_GPIO_SetPinMode(FLOCKLAB_LED2_PORT, FLOCKLAB_LED2_PIN, LL_GPIO_MODE_OUTPUT);
+#endif /* USE_SWO */
+#if !USE_SWD
+  /* do not configure the SWDIO / SWDCLK pins as outputs if SWD debugging is to be used */
+  LL_GPIO_SetPinMode(FLOCKLAB_LED3_PORT, FLOCKLAB_LED3_PIN, LL_GPIO_MODE_OUTPUT);
+  LL_GPIO_SetPinMode(FLOCKLAB_INT2_PORT, FLOCKLAB_INT2_PIN, LL_GPIO_MODE_OUTPUT);
+#endif /* USE_SWD */
+  LL_GPIO_SetPinMode(FLOCKLAB_INT1_PORT, FLOCKLAB_INT1_PIN, LL_GPIO_MODE_OUTPUT);
 }
 
 
@@ -86,6 +178,8 @@ void uart_init(void)
 {
   /* configure pins */
   LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+  LL_GPIO_SetPinMode(UART_RXD_PORT, UART_RXD_PIN, LL_GPIO_MODE_ALTERNATE);
+  LL_GPIO_SetPinMode(UART_TXD_PORT, UART_TXD_PIN, LL_GPIO_MODE_ALTERNATE);
   LL_GPIO_SetAFPin_8_15(UART_RXD_PORT, UART_RXD_PIN, GPIO_AF7_USART1);
   LL_GPIO_SetAFPin_8_15(UART_TXD_PORT, UART_TXD_PIN, GPIO_AF7_USART1);
   LL_GPIO_SetPinSpeed(UART_RXD_PORT, UART_RXD_PIN, LL_GPIO_SPEED_FREQ_HIGH);
@@ -94,25 +188,28 @@ void uart_init(void)
   LL_GPIO_SetPinOutputType(UART_TXD_PORT, UART_TXD_PIN, LL_GPIO_OUTPUT_PUSHPULL);
   LL_GPIO_SetPinPull(UART_RXD_PORT, UART_RXD_PIN, LL_GPIO_PULL_UP);
   LL_GPIO_SetPinPull(UART_TXD_PORT, UART_TXD_PIN, LL_GPIO_PULL_UP);
-  LL_GPIO_SetPinMode(UART_RXD_PORT, UART_RXD_PIN, LL_GPIO_MODE_ALTERNATE);
-  LL_GPIO_SetPinMode(UART_TXD_PORT, UART_TXD_PIN, LL_GPIO_MODE_ALTERNATE);
 
   /* enable clock and set clock source */
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1);
-  LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);
+  LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);  /* = CPU_SPEED */
 
   /* configure module */
   LL_USART_Disable(USART1);
-  //LL_USART_ConfigAsyncMode(USART1);
+  LL_USART_ConfigAsyncMode(USART1);
   LL_USART_SetTransferDirection(USART1, LL_USART_DIRECTION_TX_RX);
-  LL_USART_ConfigCharacter(USART1, LL_USART_DATAWIDTH_8B, LL_USART_PARITY_NONE, LL_USART_STOPBITS_1);      /* 8 data bit, 1 start bit, 1 stop bit, no parity */
+  /* 8 data bit, 1 start bit, 1 stop bit, no parity */
+  LL_USART_ConfigCharacter(USART1, LL_USART_DATAWIDTH_8B, LL_USART_PARITY_NONE, LL_USART_STOPBITS_1);
   LL_USART_SetHWFlowCtrl(USART1, LL_USART_HWCONTROL_NONE);
-  LL_USART_SetOverSampling(USART1, LL_USART_OVERSAMPLING_16);
-  LL_USART_SetBaudRate(USART1, SystemCoreClock, LL_USART_OVERSAMPLING_16, 115200);
+  uint32_t mode = LL_USART_OVERSAMPLING_16;
+  if (CPU_SPEED / UART_BAUDRATE < 16) {
+    mode = LL_USART_OVERSAMPLING_8;
+  }
+  LL_USART_SetOverSampling(USART1, mode);
+  LL_USART_SetBaudRate(USART1, SystemCoreClock, mode, UART_BAUDRATE);
   LL_USART_Enable(USART1);
 
   /* wait until the module is active */
-  while((!(LL_USART_IsActiveFlag_TEACK(USART1))) || (!(LL_USART_IsActiveFlag_REACK(USART1))));
+  while ((!(LL_USART_IsActiveFlag_TEACK(USART1))) || (!(LL_USART_IsActiveFlag_REACK(USART1))));
 }
 
 
@@ -131,59 +228,38 @@ void uart_println(const char* str)
 }
 
 
-void delay(uint32_t loops)
+void delay(volatile uint32_t loops)
 {
-  while(loops) loops--;
+  while (loops) loops--;
 }
 
 
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follows :
-  *            System Clock source            = PLL (MSI)
-  *            SYSCLK(Hz)                     = 80000000
-  *            HCLK(Hz)                       = 80000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 1
-  *            APB2 Prescaler                 = 1
-  *            MSI Frequency(Hz)              = 4000000
-  *            PLL_M                          = 1
-  *            PLL_N                          = 40
-  *            PLL_R                          = 2
-  *            Flash Latency(WS)              = 4
-  * @param  None
-  * @retval None
-  */
 void clock_init(void)
 {
   /* MSI configuration and activation */
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_4);
   LL_RCC_MSI_Enable();
-  while(!LL_RCC_MSI_IsReady());
+  while (!LL_RCC_MSI_IsReady());
 
   /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_MSI, LL_RCC_PLLM_DIV_1, 40, LL_RCC_PLLR_DIV_2);
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_MSI, LL_RCC_PLLM_DIV_1, (uint32_t)40 * CPU_SPEED / 80000000, LL_RCC_PLLR_DIV_2);  // 40 = 80MHz, 24 = 48MHz
   LL_RCC_PLL_Enable();
   LL_RCC_PLL_EnableDomain_SYS();
-  while(!LL_RCC_PLL_IsReady());
+  while (!LL_RCC_PLL_IsReady());
 
   /* Sysclk activation on the main PLL */
   LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
   LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
 
   /* Set APB1 & APB2 prescaler*/
   LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
   LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
 
-  /* Set systick to 1ms in using frequency set to 80MHz */
-  /* This frequency can be calculated through LL RCC macro */
-  /* ex: __LL_RCC_CALC_PLLCLK_FREQ(__LL_RCC_CALC_MSI_FREQ(LL_RCC_MSIRANGESEL_RUN, LL_RCC_MSIRANGE_6),
-                                  LL_RCC_PLLM_DIV_1, 40, LL_RCC_PLLR_DIV_2)*/
-  LL_Init1msTick(80000000);
+  LL_Init1msTick(CPU_SPEED);
 
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
-  LL_SetSystemCoreClock(80000000);
+  LL_SetSystemCoreClock(CPU_SPEED);
 }
 
 
